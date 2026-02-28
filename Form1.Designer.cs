91c7d08ccd5d3f3c8afa176fca06735268e718dc
Ianerrolsdelossantos <ianerrolsdelossantos@gmail.com>
2026-02-28 23:37:23 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Laboratory_Exercise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblMiddelName = new System.Windows.Forms.Label();
+            this.lblBirthDate = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtMiddelName = new System.Windows.Forms.TextBox();
+            this.rdoMale = new System.Windows.Forms.RadioButton();
+            this.rdoFemale = new System.Windows.Forms.RadioButton();
+            this.cboDay = new System.Windows.Forms.ComboBox();
+            this.cboMonth = new System.Windows.Forms.ComboBox();
+            this.cboYear = new System.Windows.Forms.ComboBox();
+            this.btnregister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.BackColor = System.Drawing.Color.Transparent;
+            this.lblFirstName.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstName.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblFirstName.Location = new System.Drawing.Point(125, 127);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(84, 21);
+            this.lblFirstName.TabIndex = 0;
+            this.lblFirstName.Text = "Firstname";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.BackColor = System.Drawing.Color.Transparent;
+            this.lblLastName.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblLastName.Location = new System.Drawing.Point(126, 207);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(83, 21);
+            this.lblLastName.TabIndex = 1;
+            this.lblLastName.Text = "Lastname";
+            // 
+            // lblMiddelName
+            // 
+            this.lblMiddelName.AutoSize = true;
+            this.lblMiddelName.BackColor = System.Drawing.Color.Transparent;
+            this.lblMiddelName.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMiddelName.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblMiddelName.Location = new System.Drawing.Point(125, 277);
+            this.lblMiddelName.Name = "lblMiddelName";
+            this.lblMiddelName.Size = new System.Drawing.Size(108, 21);
+            this.lblMiddelName.TabIndex = 2;
+            this.lblMiddelName.Text = "MiddelName";
+            // 
+            // lblBirthDate
+            // 
+            this.lblBirthDate.AutoSize = true;
+            this.lblBirthDate.BackColor = System.Drawing.Color.Transparent;
+            this.lblBirthDate.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBirthDate.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblBirthDate.Location = new System.Drawing.Point(126, 482);
+            this.lblBirthDate.Name = "lblBirthDate";
+            this.lblBirthDate.Size = new System.Drawing.Size(112, 21);
+            this.lblBirthDate.TabIndex = 4;
+            this.lblBirthDate.Text = "Date Of Birth";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblTitle.Location = new System.Drawing.Point(126, 64);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(182, 21);
+            this.lblTitle.TabIndex = 5;
+            this.lblTitle.Text = "Student Registry Form";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.BackColor = System.Drawing.Color.Transparent;
+            this.lblGender.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGender.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblGender.Location = new System.Drawing.Point(126, 378);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(66, 21);
+            this.lblGender.TabIndex = 3;
+            this.lblGender.Text = "Gender";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(119, 151);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(291, 20);
+            this.txtFirstName.TabIndex = 6;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(119, 232);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(301, 20);
+            this.txtLastName.TabIndex = 7;
+            // 
+            // txtMiddelName
+            // 
+            this.txtMiddelName.Location = new System.Drawing.Point(119, 322);
+            this.txtMiddelName.Name = "txtMiddelName";
+            this.txtMiddelName.Size = new System.Drawing.Size(301, 20);
+            this.txtMiddelName.TabIndex = 8;
+            // 
+            // rdoMale
+            // 
+            this.rdoMale.AutoSize = true;
+            this.rdoMale.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoMale.Location = new System.Drawing.Point(130, 423);
+            this.rdoMale.Name = "rdoMale";
+            this.rdoMale.Size = new System.Drawing.Size(65, 25);
+            this.rdoMale.TabIndex = 9;
+            this.rdoMale.TabStop = true;
+            this.rdoMale.Text = "Male";
+            this.rdoMale.UseVisualStyleBackColor = true;
+            this.rdoMale.CheckedChanged += new System.EventHandler(this.rdoMale_CheckedChanged);
+            // 
+            // rdoFemale
+            // 
+            this.rdoFemale.AutoSize = true;
+            this.rdoFemale.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoFemale.Location = new System.Drawing.Point(237, 423);
+            this.rdoFemale.Name = "rdoFemale";
+            this.rdoFemale.Size = new System.Drawing.Size(82, 25);
+            this.rdoFemale.TabIndex = 10;
+            this.rdoFemale.TabStop = true;
+            this.rdoFemale.Text = "Female";
+            this.rdoFemale.UseVisualStyleBackColor = true;
+            this.rdoFemale.CheckedChanged += new System.EventHandler(this.rdoFemale_CheckedChanged);
+            // 
+            // cboDay
+            // 
+            this.cboDay.FormattingEnabled = true;
+            this.cboDay.Location = new System.Drawing.Point(110, 520);
+            this.cboDay.Name = "cboDay";
+            this.cboDay.Size = new System.Drawing.Size(121, 21);
+            this.cboDay.TabIndex = 11;
+            // 
+            // cboMonth
+            // 
+            this.cboMonth.FormattingEnabled = true;
+            this.cboMonth.Location = new System.Drawing.Point(237, 520);
+            this.cboMonth.Name = "cboMonth";
+            this.cboMonth.Size = new System.Drawing.Size(121, 21);
+            this.cboMonth.TabIndex = 12;
+            // 
+            // cboYear
+            // 
+            this.cboYear.FormattingEnabled = true;
+            this.cboYear.Location = new System.Drawing.Point(364, 520);
+            this.cboYear.Name = "cboYear";
+            this.cboYear.Size = new System.Drawing.Size(121, 21);
+            this.cboYear.TabIndex = 13;
+            // 
+            // btnregister
+            // 
+            this.btnregister.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.btnregister.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnregister.ForeColor = System.Drawing.Color.Crimson;
+            this.btnregister.Location = new System.Drawing.Point(232, 588);
+            this.btnregister.Name = "btnregister";
+            this.btnregister.Size = new System.Drawing.Size(126, 38);
+            this.btnregister.TabIndex = 14;
+            this.btnregister.Text = "Register";
+            this.btnregister.UseVisualStyleBackColor = false;
+            this.btnregister.Click += new System.EventHandler(this.btnregister_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Laboratory_Exercise.Properties.Resources._442365f83c6e9b6e3248e2e67ca2a3fe;
+            this.ClientSize = new System.Drawing.Size(821, 850);
+            this.Controls.Add(this.btnregister);
+            this.Controls.Add(this.cboYear);
+            this.Controls.Add(this.cboMonth);
+            this.Controls.Add(this.cboDay);
+            this.Controls.Add(this.rdoFemale);
+            this.Controls.Add(this.rdoMale);
+            this.Controls.Add(this.txtMiddelName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblBirthDate);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblMiddelName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblMiddelName;
+        private System.Windows.Forms.Label lblBirthDate;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtMiddelName;
+        private System.Windows.Forms.RadioButton rdoMale;
+        private System.Windows.Forms.RadioButton rdoFemale;
+        public System.Windows.Forms.ComboBox cboDay;
+        public System.Windows.Forms.ComboBox cboMonth;
+        public System.Windows.Forms.ComboBox cboYear;
+        public System.Windows.Forms.Button btnregister;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratory_Exercise
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+            for (int day = 1; day <= 31; day++)
+            {
+                cboDay.Items.Add(day);
+            }
+
+           
+            for (int month = 1; month <= 12; month++)
+            {
+                cboMonth.Items.Add(month);
+            }
+
+            
+            int currentYear = DateTime.Now.Year;
+
+            for (int year = 1900; year <= currentYear; year++)
+            {
+                cboYear.Items.Add(year);
+            }
+        }
+        
+        
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdoMale_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdoFemale_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnregister_Click(object sender, EventArgs e)
+        {
+            string firstName = txtFirstName.Text;
+            string lastName = txtLastName.Text;
+            string middleName = txtMiddelName.Text;
+
+            // Decision statement for Gender
+            string gender = "";
+
+            if (rdoMale.Checked)
+            {
+                gender = "Male";
+            }
+            else if (rdoFemale.Checked)
+            {
+                gender = "Female";
+            }
+            else
+            {
+                gender = "Not Selected";
+            }
+
+            // Decision statement for ComboBoxes
+            string day = "";
+            string month = "";
+            string year = "";
+
+            if (cboDay.SelectedItem != null)
+                day = cboDay.SelectedItem.ToString();
+
+            if (cboMonth.SelectedItem != null)
+                month = cboMonth.SelectedItem.ToString();
+
+            if (cboYear.SelectedItem != null)
+                year = cboYear.SelectedItem.ToString();
+
+            string birthDate = month + "/" + day + "/" + year;
+
+            // Display all information
+            MessageBox.Show(
+                "Student Information\n\n" +
+                "First Name: " + firstName + "\n" +
+                "Last Name: " + lastName + "\n" +
+                "Middle Name: " + middleName + "\n" +
+                "Gender: " + gender + "\n" +
+                "Date of Birth: " + birthDate,
+                "Registration Summary",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+    }
+}
+        
+    
+
